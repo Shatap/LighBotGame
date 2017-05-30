6d49804bc7287b969af9272fbd3d003c3646a822
shatap <yamin.tricha@etu.u-bordeaux.fr>
2017-05-30 14:30:10 +0200

    fix

--- LightGame/robot.cpp
+++ LightGame/robot.cpp
@@ -2,8 +2,8 @@
 
 Robot::Robot():_rate{0}
 {
-    //_robot.setOutlineColor(sf::Color::Green);
-    //_robot.setOutlineThickness(4);
+    _robot.setOutlineColor(sf::Color::Green);
+    _robot.setOutlineThickness(4);
     _robot.setRadius(20);
     _robot.setPosition({60,80});
     _robot.setOrigin({20,20});
@@ -16,9 +16,9 @@ Robot::Robot():_rate{0}
 
 void Robot::draw_bot(sf::RenderWindow &w)
 {
-    sf::Texture texture;
-    texture.loadFromFile("../LightGame/Sources/robot.png");
-    _robot.setTexture(&texture);
+    //sf::Texture texture;
+    //texture.loadFromFile("../LightGame/Sources/robot.png");
+    //_robot.setTexture(&texture);
     w.draw(_robot);
 }
 
@@ -49,6 +49,7 @@ void Robot::lightHex(Grid &g, sf::RenderWindow &w)
 void Robot::setPosition()
 {
 
+
     switch(_rate)
     {
     case 0:
@@ -59,19 +60,16 @@ void Robot::setPosition()
     case 1:
     case -1:
         _robot.setPosition({getPosition().x,getPosition().y+(30*sin((PI)/3)+28)});
-
         break;
 
     case 2:
     case -2:
         _robot.setPosition({getPosition().x-((30*cos(PI/3))+30),getPosition().y+(30*sin((PI)/3))});
-
         break;
 
     case 3:
     case -3:
         _robot.setPosition({getPosition().x-((30*cos(PI/3))+30),getPosition().y-(30*sin((PI)/3))});
-
         break;
 
     case 4:
@@ -82,30 +80,10 @@ void Robot::setPosition()
     case 5:
     case -5:
         _robot.setPosition({getPosition().x+(30*cos(PI/3))+30,getPosition().y-(30*sin((PI)/3))});
-
-
         break;
 
     }
 
-    if(_rate == 0 )
-    {
-    }
-
-    else if(_rate == 1|| _rate == -1)
-    {
-
-
-    }
-    else if(_rate == 2 || _rate == -2)
-    {
-
-    }
-
-    else  if(_rate == 3)
-    {
-
-    }
 
 
 
